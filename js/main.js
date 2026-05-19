7d8e4d66074a731a9e57ca80c1f4b2130c9b49a4
Kaylee Faherty <Kayleefaherty@gmail.com>
2026-05-19 13:53:21 -0400

    finalize homepage HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const timelineItems = document.querySelectorAll(".timeline-item");
-timelineItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    const content = item.querySelector(".timeline-description");
-    content.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", () => {
+  const timelineItems = document.querySelectorAll(".timeline-item");
+  timelineItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      const content = item.querySelector(".timeline-description");
+      content.classList.toggle("active");
+    });
   });
 });
