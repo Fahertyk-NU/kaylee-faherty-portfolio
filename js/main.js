ff8f345522e3f52eb7faaebaf0d98f325f5d70c4
Kaylee Faherty <Kayleefaherty@gmail.com>
2026-05-19 12:32:52 -0400

    add timeline CSS styling

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const timelineItems = document.querySelectorAll(".timeline-item");
+timelineItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    const content = item.querySelector(".timeline-description");
+    content.classList.toggle("active");
+  });
+});
